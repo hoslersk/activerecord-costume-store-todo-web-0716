d3e68b0bac99f9ce682b8a075b4386916f7b3efb
Schyler Hosler <schylerhosler@gmail.com>
2016-08-08 20:09:00 -0400

    Initial commit:

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,14 @@
 # Create your HauntedHouse class here
+
+class HauntedHouse < ActiveRecord::Base
+
+  # name
+  # location
+  # theme
+  # price
+  # whether they're family friendly or not
+  # opening date
+  # closing date
+  # long description
+
+end
